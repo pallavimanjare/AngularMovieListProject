c750d51b6f3631d2b99ae785379719521219673c
pallavi.udhane <pallavi.udhane@gibsonstechnologies.com>
2021-05-19 16:29:13 +0530

    added model for movie to read movies json

---
+++ src/app/movie.ts
@@ -0,0 +1,14 @@
+export class Movie {
+
+    id: number = -1;
+ 
+    location: string= "";
+    language : string="";
+    plot: string= "";
+    poster: string= "";
+    title: string= "";
+    imdbRating: string= "";
+    listingType: string= "";
+    
+
+}
