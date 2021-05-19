e8757b444cad292c742dce456eb2ced768488cf7
pallavi.udhane <pallavi.udhane@gibsonstechnologies.com>
2021-05-19 16:42:24 +0530

    design and develop movie card

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# AngularMovieListProject
+This is a project to display a list of movies which are picked from a json file. The features include:
+1. Sorting the movies based on movie title, release date year or movie budget.
+2. Searching for a movie based on title of the movie.
+
+# MovieDisplay
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.1.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+=======
+# movie-details-angular
+A list of movies with their details.
+The movie details are picked from a local json file. 
+Features are to search the list of movies or to sort them on the basis of their budget, title, etc.
+>>>>>>> 07c4923c0ae6c920a192002de5442bc107b414cc
---
+++ src/app/app.component.ts
@@ -0,0 +1,123 @@
+import { Component } from '@angular/core';
+import { Movie } from './movie';
+import moviesList from '../app/moviesList.json';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MovieDialogComponent } from './movie-dialog/movie-dialog.component';
+
+
+export interface DialogData{
+  movie: Movie
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: []
+})
+export class AppComponent {
+
+  shouldShowPopUp = false;
+  popupMovie = null;
+  all_ratings = ["TV-Y", "TV-Y7","TV-G","G","TV-PG","PG","TV-14","PG-13","R","NC-17","TV-MA"];
+  ratings = this.all_ratings.map((item, index) => ({
+    rating: item,
+    val : index + 1
+  }))
+  title = 'movie-display';
+  movies: Movie[] = new Array();
+  display_movies: Movie[] ;
+ api_keys:string[] = ["movie_title", "director_name", "actor_1_name", "actor_2_name", "genres" , "language",
+"country", "content_rating", "budget", "title_year", "plot_keywords", "movie_imdb_link"];
+  
+
+  constructor( public dialog : MatDialog){
+
+
+   }
+
+   openDialog() : void {
+    //  let mov = this.popupMovie;
+     const dialogRef = this.dialog.open(MovieDialogComponent, {
+      //  width: '250px',
+      width : '90%',
+       data: { movie: this.popupMovie } 
+     });
+     dialogRef.afterClosed().subscribe(result =>{
+       console.log('The dialog was closed.');
+
+     });
+   }
+
+   showCard(movie: Movie){
+     console.log("shoCard" + movie);
+     if(movie != null){
+      // this.shouldShowPopUp = true;
+      this.popupMovie = movie;
+      this.openDialog();
+     }
+   }
+  getRatingValue(rating: string)
+  {
+    for(let json in this.ratings)
+    {
+      
+      if(this.ratings[json]["rating"] == rating)
+      {
+        return this.ratings[json]["val"];
+      }
+    }
+    return NaN;
+  }
+  searchString(searchs : string)
+  {
+    this.display_movies = this.movies.filter(movie => movie.title.toLowerCase().indexOf(searchs.toLowerCase()) != -1)
+  }
+  sort_by_emitter(sorter : string)
+  {
+    console.log("sort1 called.");
+    if(sorter === "All")
+    {
+      this.display_movies = this.movies;
+    }
+    else if(sorter === "Rating: Low to High")
+      this.display_movies = this.movies.sort((a,b) => (isNaN(parseInt(a.imdbRating)) ? 0 : parseInt(a.imdbRating)  ) 
+      - (isNaN(parseInt(b.imdbRating) )? 0 : parseInt(b.imdbRating)));
+    else if(sorter === "Location: High to Low")
+    this.display_movies = this.movies.sort((a,b) => (isNaN(parseInt(a.location)) ? 0 : parseInt(a.location)  ) 
+    - (isNaN(parseInt(b.location) )? 0 : parseInt(b.location))).reverse();
+    else if(sorter === "Language: Latest First")
+      this.display_movies = this.movies.sort((a,b) => (isNaN(parseInt(a.language)) ? 0 : parseInt(a.language)) 
+      - (isNaN(parseInt(b.language)) ? 0 : parseInt(b.language))).reverse();
+    else if(sorter === "Title : Asc to Desc")
+      this.display_movies = this.movies.sort((a,b) => (isNaN(parseInt(a.title)) ? 0 : parseInt(a.title)) 
+      - (isNaN(parseInt(b.language)) ? 0 : parseInt(b.language)));
+    
+  }
+
+  ngOnInit(){
+    this.getMovies();
+
+  }
+
+  getMovies(){
+    
+    console.log("API Hit.");
+     //Serve the JSON file to the 
+        this.display_movies = moviesList.map(json => 
+          Object.assign(new Movie, {         
+            language: json.Language,
+            location: json.Location,
+            plot:json.Plot,
+            poster: json.Poster,
+            title:json.Title,
+            imdbRating: json.imdbRating,
+            listingType: json.listingType
+
+          }));
+        this.movies = this.display_movies;
+        
+      
+  }
+
+}
